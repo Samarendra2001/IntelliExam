a81e2293a515a23f76d4d562fe3ce1c8410674c2
Samrendra Barick <baricksamarendra@gmail.com>
2024-01-17 13:32:18 +0530

    SignIn api created

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from 'bcryptjs'
 import User from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/Error.js';
 
 export const signup = async(req,res,next)=>{
     const{name,email,password} = req.body;
@@ -10,5 +12,22 @@ export const signup = async(req,res,next)=>{
         res.status(201).json('User created successfully');
     }catch(error){
         next(error);
+    }
+}
+
+export const signin = async(req,res,next)=>{
+    const {email,password} = req.body;
+    try{
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(errorHandler(404,'User not Found'));
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword) return next (errorHandler(401,'wrong Credentials'));
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const {passwod:pass, ...rest} = validUser._doc;
+        res.cookie('access_token',token,{htttpOnly:true}).status(200).json(rest);
+    }catch(error){
+        next(error)
     }
 }
