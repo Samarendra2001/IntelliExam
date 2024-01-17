d809dc60ac0f3572c9052c99d27cc827a7f68402
Samrendra Barick <baricksamarendra@gmail.com>
2024-01-17 07:56:47 +0530

    First creating a test Api route

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,6 @@
+const test = (req,res)=>{
+    res.json({
+        message:'Our Test Api is working fine',
+    })
+}
+export default test;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from "./routes/user.route.js"
 dotenv.config();//initialization of dotenv
 
 //connection to DB
@@ -15,3 +16,8 @@ const app = express();
 app.listen(5000,()=>{
     console.log("server is running on port 5000")
 })
+
+
+//defining api route of user router
+
+app.use('/api/user',userRouter);
